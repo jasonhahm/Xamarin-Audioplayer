916ec5403e53e3e789cfb1fe164c19eb45d3aa85
Jason Hahm <hahm95@gmail.com>
2018-07-08 18:10:29 -0500

    Initial commit

---
+++ AudioPlayer/ViewModels/AudioPlayerViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using AudioPlayer.Services;
+using Xamarin.Forms;
+
+namespace AudioPlayer.ViewModels
+{
+	public class AudioPlayerViewModel: INotifyPropertyChanged
+	{
+        List<string> playlist = new List<string>
+            { "Galway.mp3", "Bank.mp3", "Air.mp3" };
+
+        int index = 0;
+
+        private IAudioPlayerService _audioPlayer;
+		private bool _isStopped;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public AudioPlayerViewModel(IAudioPlayerService audioPlayer)
+		{
+			_audioPlayer = audioPlayer;
+			_audioPlayer.OnFinishedPlaying = () => {
+				_isStopped = true;
+				CommandText = "Play";
+			};
+			CommandText = "Play";
+            FileText = playlist[index];
+			_isStopped = true;
+		}
+
+		private string _commandText;
+		public string CommandText
+		{
+			get { return _commandText;}
+			set
+			{
+				_commandText = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CommandText"));
+			}
+		}
+
+        private string _fileText;
+        public string FileText
+        {
+            get { return _fileText; }
+            set
+            {
+                _fileText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FileText"));
+            }
+        }
+
+        private ICommand _playPauseCommand;
+		public ICommand PlayPauseCommand
+		{
+			get
+			{
+				return _playPauseCommand ?? (_playPauseCommand = new Command(
+					(obj) => 
+				{
+					if (CommandText == "Play")
+					{
+						if (_isStopped)
+						{
+							_isStopped = false;
+							_audioPlayer.Play(playlist[index]);
+						}
+						else
+						{
+							_audioPlayer.Play();
+						}
+						CommandText = "Pause";
+					}
+					else
+					{
+						_audioPlayer.Pause();
+						CommandText = "Play";
+					}
+				}));
+			}
+		}
+
+        private ICommand _PrevCommand;
+        public ICommand PrevCommand
+        {
+            get
+            {
+                return _PrevCommand ?? (_PrevCommand = new Command(
+                    (obj) =>
+                    {
+                         if (index>0)
+                         {
+                            index = index - 1;
+                            _audioPlayer.Play(playlist[index]);
+                            FileText = playlist[index];
+                        } else
+                         {
+                                    
+                         } 
+                    }));
+            }
+        }
+
+        private ICommand _NextCommand;
+        public ICommand NextCommand
+        {
+            get
+            {
+                return _NextCommand ?? (_NextCommand = new Command(
+                    (obj) =>
+                    {
+                        if (index < playlist.Count -1 )
+                        {
+                            index = index + 1;
+                            _audioPlayer.Play(playlist[index]);
+                            FileText = playlist[index];
+                        }
+                        else
+                        {
+
+                        }
+                    }));
+            }
+        }
+    }
+}
---
+++ Droid/obj/Debug/android/src/md5dfb2769c5e34fd0ae04246f56d9c7b1c/MainActivity.java
@@ -0,0 +1,47 @@
+package md5dfb2769c5e34fd0ae04246f56d9c7b1c;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AudioPlayer.Droid.MainActivity, AudioPlayer.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AudioPlayer.Droid.MainActivity, AudioPlayer.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
